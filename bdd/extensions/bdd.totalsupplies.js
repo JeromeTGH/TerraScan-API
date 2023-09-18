dfb726bc2fc973c228dc3e45d9c33536d95db256
JeromeTGH <jerome.tomski@gmx.fr>
2023-09-18 10:58:34 +0200

    Ajout de fonctions de test BDD, table 'tblTotalSupplies'

--- bdd/extensions/bdd.totalsupplies.js
+++ bdd/extensions/bdd.totalsupplies.js
@@ -64,6 +64,54 @@ export const totalsupplies = {
         );`
 
         return await myquery(rqt);
+    },
+
+    async selectLastH1() {
+        const rqt = `SELECT MAX(enregNumber) FROM tblTotalSupplies WHERE bH1=TRUE;`
+        return await myquery(rqt);
+    },
+
+    async enregistreTotalSuppliesDansTable(uluna, uusd) {
+
+        const maintenant = new Date();
+        const date_formatee = maintenant.toISOString().slice(0, 19).replace('T', ' ');
+    
+        const nAnnee = date_formatee.slice(0, 4);
+        const nMois = date_formatee.slice(5, 7);
+        const nJour = date_formatee.slice(8, 10);
+        const nHeure = date_formatee.slice(11, 13);
+        const nMinute = date_formatee.slice(14, 16);
+    
+        const dayOfTheWeek = maintenant.getUTCDay();        // 0 for Sunday, 1 for Monday, 2 for Tuesday, and so on
+    
+    
+        const code = nAnnee + nMois + nJour + nHeure + nMinute;
+        const datetimeUTC = date_formatee;
+        const bH1 = true;
+        const bH4 = (nHeure === 0 || nHeure === 4 || nHeure === 8 || nHeure === 12 || nHeure === 16 || nHeure === 20);
+        const bD1 = nHeure === 0;
+        const bW1 = (dayOfTheWeek === 1 && nHeure === 0);       // Lundi à Oh...
+        const bM1 = (nJour === 1 && nHeure === 0);
+        const bY1 = (nMois === 1 && nJour === 1 && nHeure === 0);
+        const ulunaAmount = uluna;
+        const uusdAmount = uusd;
+    
+        
+        const rqt = `INSERT INTO tblTotalSupplies VALUES (
+            null,
+            '${code}',
+            '${datetimeUTC}',
+            ${bH1},
+            ${bH4},
+            ${bD1},
+            ${bW1},
+            ${bM1},
+            ${bY1},
+            ${ulunaAmount},
+            ${uusdAmount}
+        );`
+    
+        return await myquery(rqt);
     }
     
 
