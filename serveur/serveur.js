f581ab2f2dc4cec2fb61579e89a3c0ece3bbd177
JeromeTGH <jerome.tomski@gmx.fr>
2023-10-25 10:38:41 +0200

    Authorisation d'accès à l'API élargie avec une whitelist (via Cors)

--- serveur/serveur.js
+++ serveur/serveur.js
@@ -5,13 +5,33 @@ const routesLuncStaking = require('./routes/routes.luncstaking.js');
 const routesCommunityPool = require('./routes/routes.communitypool.js');
 const routesOraclePool = require('./routes/routes.oraclepool.js');
 const logger = require('../utils/logger.js');
+const cors = require('cors');
 
 const start = () => {
 
     // Création du serveur
     const app = express();
 
+    // Cors
+    const tblCorsWhiteList = process.env.CORS_WHITELIST.split(/\s*,\s*/);
+    const corsOptionsDelegate = (req, callback) => {
+        const corsOptions = {
+            methods: ["GET", "PUT", "POST", "DELETE", "HEAD", "PATCH"],
+            allowedHeaders: ["Content-Type", "Authorization"],
+            credentials: true
+        };
+
+        const myIpAddress = req.connection.remoteAddress;           // Récupération de l'adresse IP du requéteur
+        if (tblCorsWhiteList.indexOf(myIpAddress) !== -1) {
+            corsOptions.origin = true
+        } else {
+            corsOptions.origin = false
+        }
+        callback(null, corsOptions);
+    }
+    
     // Middlewares
+    app.use(cors(corsOptionsDelegate));
     app.use(express.json());                            // Parse application/json
 	app.use(express.urlencoded({ extended: true }));	// Parse application/x-www-form-urlencoded	
 
