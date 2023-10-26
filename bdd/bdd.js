eeec95cc637a94df285b8fcb8c255f9b8d6ae19c
JeromeTGH <jerome.tomski@gmx.fr>
2023-10-26 09:07:15 +0200

    Reprise en place du filtre inversant, au niveau des requêtes SQL

--- bdd/bdd.js
+++ bdd/bdd.js
@@ -30,31 +30,31 @@ const myquery = async (sql) => {
 
 // Externe : permet de récupérer des X dernières valeurs de TotalSupplies ; avec la plus récente en premier, et trié par unité de temps souhaité (H1, H4, D1, ...)
 const getTotalSupplies = async(limit, timeunit) => {
-    const rqt = `SELECT * FROM tblTotalSupplies WHERE b${timeunit}=TRUE ORDER BY enregNumber ASC LIMIT ${limit}`;
+    const rqt = `SELECT * FROM tblTotalSupplies WHERE b${timeunit}=TRUE ORDER BY enregNumber DESC LIMIT ${limit}`;
     return await myquery(rqt);
 }
 
 // Externe : permet de récupérer des X dernières valeurs de TotalSupplies ; avec la plus récente en premier, et trié par unité de temps souhaité (H1, H4, D1, ...)
 const getTotalSupplies2 = async(limit, timeunit) => {
-    const rqt = `SELECT * FROM tblTotalSupplies2 WHERE b${timeunit}=TRUE ORDER BY enregNumber ASC LIMIT ${limit}`;
+    const rqt = `SELECT * FROM tblTotalSupplies2 WHERE b${timeunit}=TRUE ORDER BY enregNumber DESC LIMIT ${limit}`;
     return await myquery(rqt);
 }
 
 // Externe : permet de récupérer des X dernières valeurs de LuncStaking ; avec la plus récente en premier, et trié par unité de temps souhaité (H1, H4, D1, ...)
 const getLuncStaking = async(limit, timeunit) => {
-    const rqt = `SELECT * FROM tblLuncStaking WHERE b${timeunit}=TRUE ORDER BY enregNumber ASC LIMIT ${limit}`;
+    const rqt = `SELECT * FROM tblLuncStaking WHERE b${timeunit}=TRUE ORDER BY enregNumber DESC LIMIT ${limit}`;
     return await myquery(rqt);
 }
 
 // Externe : permet de récupérer des X dernières valeurs de CommunityPoolContent ; avec la plus récente en premier, et trié par unité de temps souhaité (H1, H4, D1, ...)
 const getCommunityPoolContent = async(limit, timeunit) => {
-    const rqt = `SELECT * FROM tblCommunityPoolContent WHERE b${timeunit}=TRUE ORDER BY enregNumber ASC LIMIT ${limit}`;
+    const rqt = `SELECT * FROM tblCommunityPoolContent WHERE b${timeunit}=TRUE ORDER BY enregNumber DESC LIMIT ${limit}`;
     return await myquery(rqt);
 }
 
 // Externe : permet de récupérer des X dernières valeurs de OraclePoolContent ; avec la plus récente en premier, et trié par unité de temps souhaité (H1, H4, D1, ...)
 const getOraclePoolContent = async(limit, timeunit) => {
-    const rqt = `SELECT * FROM tblOraclePoolContent WHERE b${timeunit}=TRUE ORDER BY enregNumber ASC LIMIT ${limit}`;
+    const rqt = `SELECT * FROM tblOraclePoolContent WHERE b${timeunit}=TRUE ORDER BY enregNumber DESC LIMIT ${limit}`;
     return await myquery(rqt);
 }
 
